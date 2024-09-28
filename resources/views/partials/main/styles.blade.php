c9b56dc51ed1a0d1910f595c02f032c14ab1e6c4
bidang TIK <obet529nf@gmail.com>
2024-09-28 17:19:41 +0700

    update slider header full background

--- resources/views/partials/main/styles.blade.php
+++ resources/views/partials/main/styles.blade.php
@@ -1,6 +1,7 @@
 <!-- Favicon -->
 <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon/favicon.ico') }}" />
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
 
 
@@ -100,5 +101,4 @@
     }
 </style>
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
